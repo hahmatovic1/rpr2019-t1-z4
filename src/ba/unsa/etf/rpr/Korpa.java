c48e67414d1c1c0f67e51c23f64809b935b5f91a
Hadija Ahmatovic <hahmatovic1@etf.unsa.ba>
2020-11-06 14:46:24 +0100

    izmjena povratne vrijednosti metode dodajArtikl iz void u boolean.  Ispravljeno dodavanje u niz artikala

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -3,9 +3,15 @@ package ba.unsa.etf.rpr;
 public class Korpa {
     private Artikl[] artikli = new Artikl[50];
     private int brElemenata = 0;
-    /*
-    public dodajArtikl(Artikl novi){
-        if(brElemenata >=0)
+
+    public boolean dodajArtikl(Artikl novi){
+        if(brElemenata >= 0) {
+            System.out.println("Korpa puna! Ne mozete dodavati vise artikala");
+            return  false;
+        }
+        artikli[brElemenata] = new Artikl(novi.getNaziv(), novi.getCijena(), novi.getKod());
+        brElemenata = brElemenata + 1;
+        return true;
     }
-    */
+
 }
