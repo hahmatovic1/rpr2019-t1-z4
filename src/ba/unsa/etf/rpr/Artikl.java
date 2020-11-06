258013247ba4475d57b5cad426f92e821916ec00
Hadija Ahmatovic <hahmatovic1@etf.unsa.ba>
2020-11-06 14:22:49 +0100

    kreiran konstruktor klase Artikl

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -5,6 +5,11 @@ public class Artikl {
     int cijena;
     String kod;
 
+    public Artikl(String naziv, int cijena, String kod) {
+        this.naziv = naziv;
+        this.cijena = cijena;
+        this.kod = kod;
+    }
 }
 
 
