446bd86dd2d79dcc7784d187eca344087b609dba
Hadija Ahmatovic <hahmatovic1@etf.unsa.ba>
2020-11-06 14:51:32 +0100

    promjena tipa povratne vrijednosti u metodi dodajArtikl. Ispravljeno dodavanje u niz artikala

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -4,10 +4,16 @@ public class Supermarket {
     private Artikl[] artikli = new Artikl[1000];
     private int brojElemenata; //ovo ce se automatski postaviti na 0
 
-    void dodajArtikl(Artikl novi){
-        if(brojElemenata >= 1000)
+
+
+    boolean dodajArtikl(Artikl novi){
+        if(brojElemenata >= 1000) {
             System.out.println("Magacin popunjen! Ne mozete dodavati vise artikala");
-        artikli[brojElemenata] = novi;
+            return false;
+        }
+        artikli[brojElemenata] = new Artikl(novi.getNaziv(), novi.getCijena(), novi.getKod());
+        brojElemenata++;
+        return true;
     }
 
 
