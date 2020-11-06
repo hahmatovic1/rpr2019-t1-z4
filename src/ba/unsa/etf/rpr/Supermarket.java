b6aa6e9a003250251237b7d60c69b2265bea85d3
Hadija Ahmatovic <hahmatovic1@etf.unsa.ba>
2020-11-06 14:36:41 +0100

    kreirana metoda dodajArtikl u klasi Supermarket

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -3,4 +3,12 @@ package ba.unsa.etf.rpr;
 public class Supermarket {
     private Artikl[] artikli = new Artikl[1000];
     private int brojElemenata; //ovo ce se automatski postaviti na 0
+
+    void dodajArtikl(Artikl novi){
+        if(brojElemenata >= 1000)
+            System.out.println("Magacin popunjen! Ne mozete dodavati vise artikala");
+        artikli[brojElemenata] = novi;
+    }
+
+
 }
