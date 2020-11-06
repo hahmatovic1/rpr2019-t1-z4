8630f1d29f4e794fe00a1880467227a95b572904
Hadija Ahmatovic <hahmatovic1@etf.unsa.ba>
2020-11-06 14:40:25 +0100

    inicijalizacija atributa brElemenata na 0 kod obje klase

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,7 +1,11 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
-    private Artikl[] artikli = new Artikl[100];
-    private int brElemenata;
-
+    private Artikl[] artikli = new Artikl[50];
+    private int brElemenata = 0;
+    /*
+    public dodajArtikl(Artikl novi){
+        if(brElemenata >=0)
+    }
+    */
 }
